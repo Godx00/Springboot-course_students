f5825b598ba3736ceabe130d87c3776de5ffc2d3
Atharva Chougale <94557388+Godx00@users.noreply.github.com>
2024-01-30 14:08:16 +0530

    Delete repository directory

--- repository/CoursesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.excelr.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.excelr.entity.Courses;
-
-public interface CoursesRepository extends JpaRepository<Courses, Integer> {
-
-}
--- repository/StudentsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.excelr.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.excelr.entity.Students;
-
-public interface StudentsRepository extends JpaRepository<Students, Integer> {
-
-}
