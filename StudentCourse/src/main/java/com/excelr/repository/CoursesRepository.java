ad5d265ffc3fbf9cb3a2c3089fb1b588c0cb10c5
Atharva Chougale <94557388+Godx00@users.noreply.github.com>
2024-01-30 14:06:46 +0530

    Add files via upload

---
+++ StudentCourse/src/main/java/com/excelr/repository/CoursesRepository.java
@@ -0,0 +1,9 @@
+package com.excelr.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.excelr.entity.Courses;
+
+public interface CoursesRepository extends JpaRepository<Courses, Integer> {
+
+}
